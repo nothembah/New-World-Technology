fb497d4e10c4eea89da36fbc37686d2dd79206b7
nothembah <74840172+nothembah@users.noreply.github.com>
2021-07-11 14:43:54 +0200

    Fixed submission js

--- myScript.js
+++ myScript.js
@@ -1,10 +1,14 @@
 function submitButton(){
-    if(document.getElementById("general").checked){
-        alert("Thank you for your general enquiry. We'll respond soon.");
-    } else if(document.getElementById("other").checked){
-        alert("Thank you for your enquiry. We'll respond soon.");
-    } else {
-        alert("Thank you for your product enquiry. We'll respond soon.");
+
+    if(fullname.checkValidity() === true && email.checkValidity() === true && message.checkValidity() === true){
+        if(document.getElementById("general").checked){
+            alert("Thank you for your general enquiry. We'll respond soon.");
+        } else if(document.getElementById("other").checked){
+            alert("Thank you for your enquiry. We'll respond soon.");
+        } else {
+            alert("Thank you for your product enquiry. We'll respond soon.");
+        }
+
     }
 }
 
